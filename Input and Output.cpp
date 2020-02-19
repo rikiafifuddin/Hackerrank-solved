c31d4e348fee86ea2515662b5b5bbcc0facc4434
Riki Afifuddin <rikinur34@gmail.com>
2020-02-19 11:21:04 +0700

    Create Input and Output.cpp

---
+++ Input and Output.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a;
+    int b;
+    int c;
+    cin >> a >> b >> c;
+    int result = a+b+c; 
+    cout << result; 
+}
