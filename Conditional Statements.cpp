372e97e51b4ab697fba3aa817cbe996c471a635e
Riki Afifuddin <rikinur34@gmail.com>
2020-02-19 11:23:03 +0700

    Create Conditional Statements.cpp

---
+++ Conditional Statements.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    if (n==1){
+        printf("one");
+    }
+    if (n==2){
+        printf ("two");
+    }
+    if (n==3){
+        printf ("three");
+    }
+    if (n==4){
+        printf ("four");
+    }
+    if (n==5){
+        printf ("five");
+    }
+    if (n==6){
+        printf ("six");
+    }
+    if (n==7){
+        printf ("seven");
+    }
+    if (n==8){
+        printf ("eight");
+    }
+    if (n==9){
+        printf ("nine");
+    }
+    if(n>9){
+        printf ("Greater than 9");
+    }
+
+    return 0;
+}
